e0d96d37fb6eff9286d36b8e95fc9204532330b6
Faisal Imam <163755226+faisalimam1@users.noreply.github.com>
2025-07-05 22:50:47 +0530

    Update vfs.h

--- Custom Virtual File System (VFS) with In-Memory Caching/include/vfs.h
+++ Custom Virtual File System (VFS) with In-Memory Caching/include/vfs.h
@@ -1,1 +1,34 @@
+// vfs.h
+
+#ifndef VFS_H       //if its already icluded then ignore
+#define VFS_H 
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+#define MAX_FILES 100
+#define MAX_FILENAME 32
+#define MAX_FILE_SIZE 1024  // In bytes
+#define STORAGE_SIZE (MAX_FILES * MAX_FILE_SIZE)
+
+// Metadata for each file
+typedef struct {
+    char name[MAX_FILENAME];
+    int size;
+    int start_block;
+    int used;  // 0 = empty, 1 = used
+    time_t created_at;
+    time_t modified_at;
+} FileEntry;
+
+// VFS structure
+typedef struct {
+    char data[STORAGE_SIZE];         // Memory buffer (like disk)
+    FileEntry files[MAX_FILES];      // Table of files
+    int file_count;                  // Number of files used
+} VirtualFileSystem;
+
+#endif
 
